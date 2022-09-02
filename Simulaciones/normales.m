4037803c3e790bf7f9c6f4060ccda9a68d440b97
Abelino Sepulveda Estrada <60146746+asepulvede@users.noreply.github.com>
2022-09-01 19:06:12 -0500

    Create normales.m

---
+++ Simulaciones/normales.m
@@ -0,0 +1,15 @@
+N=500;
+n=100;
+X= randn(N,n);
+D1= X(:,1);
+figure(1)
+hist(D1)
+%%
+X=X'
+Dmin= min(X) %minimo por fila (de cada muestra)
+figure(2)
+hist(Dmin)
+%%
+Dmax= max(X)
+figure(3)
+hist(Dmax)
